be0dd9b157302a57e3edb7a5ba75111eea2539ee
AbdullahAlBurayh <a-wat.88@hotmail.com>
2022-05-21 00:31:11 +0300

    migrations

--- ExamApp/settings.py
+++ ExamApp/settings.py
@@ -77,11 +77,11 @@ if 'RDS_DB_NAME' in os.environ:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.postgresql',
-            'NAME': 'ebdb',
-            'USER': 'ExamAppUser',
-            'PASSWORD': 'Ab123456',
-            'HOST': 'aazmvd2e1btrnl.cpxgafg4ke2u.us-west-2.rds.amazonaws.com',
-            'PORT': 5432,
+            'NAME': os.environ['RDS_DB_NAME'],
+            'USER': os.environ['RDS_USERNAME'],
+            'PASSWORD': os.environ['RDS_PASSWORD'],
+            'HOST': os.environ['RDS_HOSTNAME'],
+            'PORT': os.environ['RDS_PORT'],
         }
     }
 else:
